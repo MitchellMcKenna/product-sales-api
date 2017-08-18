e56aecd631338f69963c19fbb62a866d29db15e3
Mitchell McKenna <mitchellmckenna@gmail.com>
2017-08-18 00:27:12 -0700

    Add migrations for Products and Orders DB Tables
    Products:
    - Adding an index on product name in case allowing filtering by name in
      the future.
    
    Orders:
    - Adding an index on product_id to optomize for joins between
      the 2 tables.
    - Adding an index on created_at to optomize for filtering products by
      date (top sellers api endpoint).
    - Adding foreign key constraint that orders for a product get deleted if
      a product is deleted.

---
+++ database/migrations/2017_08_15_203012_create_orders_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->increments('id');
+            $table->bigInteger('order_id')->unsigned();
+            $table->integer('product_id')->unsigned()->index();
+            $table->smallInteger('quantity')->unsigned();
+            $table->timestamps();
+
+            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
+
+            $table->index('created_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
